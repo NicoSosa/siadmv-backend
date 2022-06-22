c2ffc339551e2350b12a83e91b6ae35622466298
NicoSosa <nico.js69@gmail.com>
2022-06-22 14:17:35 -0300

    changes

---
+++ src/SiadMV.API/Application/Requests/KeyFact/AddUserCaseKeysFactResultsRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace SiadMV.API.Application.Requests.KeyFact
+{
+    public class AddUserCaseKeysFactResultsRequest : IRequest
+    {
+        public IList<dynamic> NewCommonExpressions { get; set; }
+        public IList<dynamic> NewKeysFact { get; set; }
+        public IList<dynamic> ModifiedKeysFact { get; set; }
+        public IList<dynamic> KeysFact { get; set; }
+    }
+}
---
+++ src/SiadMV.API/Models/KeyFact/KeysFactAndQuestionsListsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SiadMV.API.Models.Question;
+using SiadMV.ServiceBase.CQRS.SeedWork;
+using System.Collections.Generic;
+
+namespace SiadMV.API.Models.KeyFact
+{
+    public class KeysFactAndQuestionsListsViewModel : IResponse
+    {
+        public IList<KeyFactViewModel> KeysFact { get; set; }
+        public IList<QuestionViewModel> Questions { get; set; }
+    }
+}
